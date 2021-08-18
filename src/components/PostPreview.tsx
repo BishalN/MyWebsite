3c6aca99a6656c299884c9471806b71d25c7ba3c
Bishal Neupane <neupanebishal07@gmail.com>
2021-08-18 16:43:05 +0545

    css fix

--- src/components/PostPreview.tsx
+++ src/components/PostPreview.tsx
@@ -6,21 +6,21 @@ const PostPreview = ({ post }) => {
   const image = getImage(post.image);
 
   return (
-    <div className='flex flex-col sm:flex-row justify-center space-x-10'>
+    <div className='flex flex-col sm:flex-row justify-center sm:space-x-10 overflow-hidden'>
       <div>
         <Link to={post.slug}>
           <GatsbyImage
             image={image}
             alt={post.author}
-            className='cursor-pointer'
+            className='cursor-pointer max-h-72 rounded-lg'
           />
         </Link>
       </div>
       <div className='w-96'>
-        <p className='font-semibold text-gray-700 text-lg max-w-xs'>
+        <p className='font-semibold text-gray-700 text-lg w-auto'>
           {post.title}
         </p>
-        <p className='mt-3 font-normal text-gray-500'>{post.excerpt}</p>
+        <p className='mt-3 font-normal text-gray-500 w-auto'>{post.excerpt}</p>
         <p className='text-gray-400 text-base'>{post.date}</p>
         <Link to={post.slug}>
           <p className='text-blue-500'>Read Blog</p>
